00a27093cba51f659324fc2c50441c37a25f3633
Matt Traudt <sirmatt@ksu.edu>
2019-04-05 10:24:34 -0400

    Start readding tb rate/burst back to set-tc.sh

--- auto-run-scripts/set-tc.sh
+++ auto-run-scripts/set-tc.sh
@@ -10,10 +10,18 @@ fi
 HOSTNAME=$1
 DEV=$2
 LATENCY_MS=$3
+PACKET_LOSS="0.0%"
 
-COMMAND_LATENCY="dev $DEV root handle 1:0 netem delay ${LATENCY_MS}ms"
+TB_RATE=10mbit
+TB_BUCKET_SIZE=4mbit
+TB_QUEUE_LIMIT=4mbit
+
+COMMAND_LATENCY_AND_PACKETLOSS="dev $DEV root handle 1:0 netem delay ${LATENCY_MS}ms loss $PACKET_LOSS"
+COMMAND_RATELIMIT="dev $DEV parent 1:1 handle 10: tbf rate $TB_RATE buffer $TB_BUCKET_SIZE limit $TB_QUEUE_LIMIT"
+
 echo "Enabling tc changes"
 ssh $HOSTNAME "
-	sudo tc qdisc add $COMMAND_LATENCY
+	sudo tc qdisc add $COMMAND_LATENCY_AND_PACKETLOSS
+	sudo tc qdisc add $COMMAND_RATELIMIT
 	sudo tc -p qdisc ls dev $DEV
 "
